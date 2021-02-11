38f98f8594e63bb7d25d6022ffabc24cb2523ebb
jefferson lennon <jeffersonlennon1@outlook.com>
2021-02-11 20:45:39 -0300

    primeira migration será de sale

---
+++ src/database/migrations/1613086514008-create_table_sale.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createTableSale1613086514008 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
